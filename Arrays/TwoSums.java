90c8ca4117578b4069b67c9140b23e87e4a9efd2
Ramadeepthi Badireddy <ramadeepthibadireddy@gmail.com>
2025-07-30 17:34:28 +0530

    Create TwoSums.java

---
+++ Arrays/TwoSums.java
@@ -0,0 +1,14 @@
+// LeetCode: https://leetcode.com/problems/two-sum/
+// Approach: HashMap to store index of complement
+
+public int[] twoSum(int[] nums, int target) {
+    Map<Integer, Integer> map = new HashMap<>();
+    for (int i = 0; i < nums.length; i++) {
+        int complement = target - nums[i];
+        if (map.containsKey(complement)) {
+            return new int[]{map.get(complement), i};
+        }
+        map.put(nums[i], i);
+    }
+    return new int[]{};
+}
