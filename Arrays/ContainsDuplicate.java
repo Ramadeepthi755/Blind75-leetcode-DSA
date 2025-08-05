92472b9571263196b7e3600ec20d485de945bf7c
Ramadeepthi Badireddy <ramadeepthibadireddy@gmail.com>
2025-08-05 21:49:54 +0530

    Update ContainsDuplicate.java

--- Arrays/ContainsDuplicate.java
+++ Arrays/ContainsDuplicate.java
@@ -1,5 +1,20 @@
-//Letcode:https://leetcode.com/problems/contains-duplicate/
+//My Profile : https://leetcode.com/u/Ramadeepthi/
+//Problem : Contains Duplicate 
+//Letcode Link : https://leetcode.com/problems/contains-duplicate/
 
+/* Approach :
+-Create Hash set as visited to store and check elements in array of nums
+-Hash set checks elements if it exits then prints true
+-if elements are not there is moves to hashset
+-if none element matches with elements in Hashset then prints false
+    */
+
+/*
+Time Complexity : O(n)
+Space Complexity : O(n) 
+    */
+
+//code
 class Solution {
     public boolean containsDuplicate(int[] nums) {
         Set <Integer> visited=new HashSet<>();
