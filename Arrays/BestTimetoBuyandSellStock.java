da33365336dbb54dfa3fde06fde5dc74bb981649
Ramadeepthi Badireddy <ramadeepthibadireddy@gmail.com>
2025-08-05 23:02:44 +0530

    Update BestTimetoBuyandSellStock.java

--- Arrays/BestTimetoBuyandSellStock.java
+++ Arrays/BestTimetoBuyandSellStock.java
@@ -1,6 +1,18 @@
+//My Profile : https://leetcode.com/u/Ramadeepthi/
+//Problem : Best Time to Buy and Sell Stock
 //Leetcode:https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
-//Approuch:use Dynamic programing approuch
 
+/*Approuch:
+-first we have to find min value that will be best time to buy(buy at low)
+-then have to subtract every element with min value after min element to get max profit 
+-if you get max profit in one element that will be best time for selling(sell at high)
+-print Profit */
+
+/* Time Complexity : O(n)
+    Space Complexity : O(1)
+    */
+
+//code
 class Solution {
     public int maxProfit(int[] prices) {
         int min=prices[0];
